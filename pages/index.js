c46502d7a589a34b26494ad66892475dc9b86098
Nawed Ali <nawed2611@gmail.com>
2022-07-20 02:41:05 +0530

    Fix: Add links to projects

--- pages/index.js
+++ pages/index.js
@@ -99,82 +99,88 @@ export default function Index() {
         <p>Check out My <a className="hover:underline hover:opacity-80" href="https://github.com/nawed2611">GitHub</a> and <a className="hover:underline hover:opacity-80" href="https://devpost.com/nawed2611">Devpost</a> for more projects!</p>
 
 
-        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0">
-          <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
-            <div className="w-full md:w-1/3 h-64 md:h-auto relative">
-              <img className='object-contain rounded-md' src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/002/010/032/datas/gallery.jpg" />
-            </div>
+        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0 hover:scale-110">
+          <a href="https://devpost.com/software/mental-peace">
+            <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
+              <div className="w-full md:w-1/3 h-64 md:h-auto relative">
+                <img className='object-contain rounded-md' src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/002/010/032/datas/gallery.jpg" />
+              </div>
 
-            <div className="bg-slate-900 rounded-md w-full md:w-2/3">
-              <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
-                <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
-                  <div className="w-2/5 text-center md:text-left">
-                    <h3 className='text-xl font-bold'>Get Sanity</h3>
-                    <p className="mb-0 mt-3 text-gray-400 text-sm italic">Holistic Mental Health Diagnosing Platform</p>
-                  </div>
+              <div className="bg-slate-900 rounded-md w-full md:w-2/3">
+                <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
+                  <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
+                    <div className="w-2/5 text-center md:text-left">
+                      <h3 className='text-xl font-bold'>Get Sanity</h3>
+                      <p className="mb-0 mt-3 text-gray-400 text-sm italic">Holistic Mental Health Diagnosing Platform</p>
+                    </div>
 
-                  <div className="w-3/5 px-3">
-                    <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
-                      Web app built for Mental Health Hacks2.0 that informs users about their mental health status through our ML model and provides them with tools to help them understand.
-                    </p>
+                    <div className="w-3/5 px-3">
+                      <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
+                        Web app built for Mental Health Hacks2.0 that informs users about their mental health status through our ML model and provides them with tools to help them understand.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
 
-        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0">
-          <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
-            <div className="w-full md:w-1/3 h-64 md:h-auto relative">
-              <img className='object-contain h-40 ml-16 rounded-md' src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/915/350/datas/gallery.jpg" />
-            </div>
+        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0 hover:scale-110">
+          <a href="https://devpost.com/software/bill-aver">
+            <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
+              <div className="w-full md:w-1/3 h-64 md:h-auto relative">
+                <img className='object-contain h-40 ml-16 rounded-md' src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/915/350/datas/gallery.jpg" />
+              </div>
+              <div className="bg-slate-900 rounded-md w-full md:w-2/3">
+                <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
+                  <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
+                    <div className="w-2/5 text-center md:text-left">
+                      <h3 className='text-xl font-bold'>Bill Saver</h3>
+                      <p className="mb-0 mt-3 text-gray-400 text-sm italic">Say hello to Savings!</p>
+                    </div>
 
-            <div className="bg-slate-900 rounded-md w-full md:w-2/3">
-              <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
-                <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
-                  <div className="w-2/5 text-center md:text-left">
-                    <h3 className='text-xl font-bold'>Bill Saver</h3>
-                    <p className="mb-0 mt-3 text-gray-400 text-sm italic">Say hello to Savings!</p>
-                  </div>
-
-                  <div className="w-3/5 px-3">
-                    <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
-                      It is an app that allows a user to see previously-stored bills be it receipts or your taxes. The app stores it in the blockchain, so it can't be tampered with at any time so that the information remains secure
-                    </p>
+                    <div className="w-3/5 px-3">
+                      <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
+                        It is an app that allows a user to see previously-stored bills be it receipts or your taxes. The app stores it in the blockchain, so it can't be tampered with at any time so that the information remains secure
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
 
-        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0">
-          <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
-            <div className="w-full md:w-1/3 h-64 md:h-auto relative">
-              <img className='object-fit h-40 rounded-md' src="https://user-images.githubusercontent.com/83456083/168167208-e5ab276c-9f7d-417d-9767-66c0c0583fce.png" />
-            </div>
+        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0 hover:scale-110">
+          <a href="https://github.com/nawed2611/News-Centre">
+            <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
+              <div className="w-full md:w-1/3 h-64 md:h-auto relative">
+                <img className='object-fit h-40 rounded-md' src="https://user-images.githubusercontent.com/83456083/168167208-e5ab276c-9f7d-417d-9767-66c0c0583fce.png" />
+              </div>
 
-            <div className="bg-slate-900 rounded-md w-full md:w-2/3">
-              <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
-                <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
-                  <div className="w-2/5 text-center md:text-left">
-                    <h3 className='text-xl font-bold'>News Centre</h3>
-                    <p className="mb-0 mt-3 text-gray-400 text-sm italic">News on your fingertips!</p>
-                  </div>
+              <div className="bg-slate-900 rounded-md w-full md:w-2/3">
+                <div className="rounded-md h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
+                  <div className="rounded-md bg-slate-800 lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center">
+                    <div className="w-2/5 text-center md:text-left">
+                      <h3 className='text-xl font-bold'>News Centre</h3>
+                      <p className="mb-0 mt-3 text-gray-400 text-sm italic">News on your fingertips!</p>
+                    </div>
 
-                  <div className="w-3/5 px-3">
-                    <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
-                      News Centre is built with React.js and NewsAPI. It provides you with "Top Headlines" from India and also has the functionality of searching any news query. News Centre also is an voice enabled AI app powered by Alan AI.
-                    </p>
+                    <div className="w-3/5 px-3">
+                      <p className="text-md mt-4 lg:mt-0 text-justify md:text-left text-sm">
+                        News Centre is built with React.js and NewsAPI. It provides you with "Top Headlines" from India and also has the functionality of searching any news query. News Centre also is an voice enabled AI app powered by Alan AI.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
 
-        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0">
+        <div className="rounded-md container mt-10 mx-auto p-4 md:p-0 hover:scale-110">
+          <a href="https://devpost.com/software/heartly-heaven">
           <div className="shadow-lg rounded-md flex flex-wrap w-full mx-auto">
             <div className="w-full md:w-1/3 h-64 md:h-auto relative">
               <img className='object-fit h-40 rounded-md' src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/832/731/datas/gallery.jpg" />
@@ -197,6 +203,7 @@ export default function Index() {
               </div>
             </div>
           </div>
+          </a>
         </div>
 
       </div>
